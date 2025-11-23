e9a0ecfb48e4df95384c9c9bccc75351230d9bb9
ESlee1006 <easonlee1006@gmail.com>
2025-11-23 08:05:25 +0800

    Update hash_fn.cpp

--- AssignmentIV/hash_fn.cpp
+++ AssignmentIV/hash_fn.cpp
@@ -1,1 +1,28 @@
+/*
+   ========================================
+   hash_fn.cpp — implement your hash functions
+   ========================================
 
+   Description:
+    This file contains the hash functions for integer and string keys.
+
+   Development History:
+    - 2025/11/11: Initial implementation
+    - 2025/11/17: Refactored to use hash_fn.hpp
+
+   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+ */
+#include "hash_fn.hpp"
+
+int myHashInt(int key, int m) {
+    key *= 97;
+    key += (key % 10);
+    return key % m;  // basic division method
+}
+
+int myHashString(const std::string& str, int m) {
+    unsigned long hash = 0;
+    for(int i = 0; i < str.size(); i++)
+        hash = hash * 31 + str[i];
+    return static_cast<int>(hash % m);  // basic division method
+}
